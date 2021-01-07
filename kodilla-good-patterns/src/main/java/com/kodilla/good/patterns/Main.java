e475c0420fcb46aa878edc61f0ab3cbcfa4103b3
jakcio <ciosekjakub@wp.pl>
2021-01-07 20:11:27 +0100

    "13.1 Task"

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Main.java
@@ -0,0 +1,15 @@
+package com.kodilla.good.patterns;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        movieStore.getStream();
+
+    }
+}
