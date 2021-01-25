2861f0a1ed97126e7cd6cc067d232cc67a7134d3
Jakub Ciosek <ciosekjakub@wp.pl>
2021-01-25 21:53:20 +0100

    "14.2 v2 task"

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -10,23 +10,23 @@ public class Calculator {
     Display display;
 
     public double add(double a, double b) {
-        display.displayValue(a+b);
-        return a+b;
+        display.displayValue(a + b);
+        return a + b;
     }
 
     public double sub(double a, double b) {
-        display.displayValue(a-b);
-        return a-b;
+        display.displayValue(a - b);
+        return a - b;
     }
 
     public double mul(double a, double b) {
-        display.displayValue(a*b);
-        return a*b;
+        display.displayValue(a * b);
+        return a * b;
     }
 
     public double div(double a, double b) {
-        display.displayValue(a/b);
-        return a/b;
+        display.displayValue(a / b);
+        return a / b;
     }
 
 
